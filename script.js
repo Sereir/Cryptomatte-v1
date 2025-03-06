a63fbe8476dbecb9cef498537b7cd196f1c7c0fc
nacimiento <nsereir.virtualbuilding.com>
2025-03-06 11:58:17 +0100

    fix globaux

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
+/* Chargement du fichier JSON */
 let jsonData = {};
 
 async function fetchData() {
   try {
     const response = await fetch('./shapes.json');
     jsonData = await response.json();
-    console.log(jsonData);
+    // console.log(jsonData);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
 fetchData();
 
+function hexToRgb(hex) {
+  const bigint = parseInt(hex, 16);
+  // console.log(hex, bigint);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  // console.log(r, g, b);
+  return { r, g, b };
+}
+
+function findColor(text) {
+  const color = jsonData[text];
+  // console.log(color);
+  if (color) {
+    return color;
+  } else {
+    alert('Forme non trouvée');
+  }
+}
+
 document.getElementById('button-search').addEventListener('click', function () {
-  const textInput = document.getElementById('color-input').value;
+  const selectElement = document.getElementById('color-input');
+  const selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value);
+  // console.log(selectedOptions)
+
   const image = document.getElementById('calque');
   const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  const color = findColor(textInput);
-  const highlightColor = hexToRgb(color);
+  /* Optimisation de la lecture des pixels */
+  const ctx = canvas.getContext('2d', { willReadFrequently: true });
+
+  const highlightColors = selectedOptions.map(findColor).map(hexToRgb);
 
   canvas.width = image.width;
   canvas.height = image.height;
@@ -31,14 +56,21 @@ document.getElementById('button-search').addEventListener('click', function () {
   const highlightCtx = highlightCanvas.getContext('2d');
   const highlightData = highlightCtx.createImageData(canvas.width, canvas.height);
   const highlightPixels = highlightData.data;
-
+  // console.log(highlightData, highlightPixels)
+  
   for (let i = 0; i < data.length; i += 4) {
-    if (data[i] === highlightColor.r && data[i + 1] === highlightColor.g && data[i + 2] === highlightColor.b) {
+    let isHighlighted = false;
+    for (const color of highlightColors) {
+      if (data[i] === color.r && data[i + 1] === color.g && data[i + 2] === color.b) {
+        isHighlighted = true;
+        break;
+      }
+    }
+    if (isHighlighted) {
       highlightPixels[i] = 255;
       highlightPixels[i + 1] = 255;
       highlightPixels[i + 2] = 0;
       highlightPixels[i + 3] = 127;
-
     } else {
       highlightPixels[i + 3] = 0;
     }
@@ -57,24 +89,5 @@ document.getElementById('button-search').addEventListener('click', function () {
   if (existingHighlight) {
     existingHighlight.remove();
   }
-
   document.querySelector('.image-container').appendChild(highlightImg);
 });
-
-function hexToRgb(hex) {
-  const bigint = parseInt(hex, 16);
-  const r = (bigint >> 16) & 255;
-  const g = (bigint >> 8) & 255;
-  const b = bigint & 255;
-  return { r, g, b };
-}
-
-function findColor(text) {
-  const color = jsonData[text];
-  console.log(color)
-  if (color) {
-    return color
-  } else {
-    alert('Shape not found');
-  }
-}
